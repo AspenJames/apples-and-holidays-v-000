7989a6f512089c4dc43657ba0d3e83af033cb526
Aspen James <aspenjames@tqca.org>
2018-03-06 01:53:15 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -60,7 +60,7 @@ def all_supplies_in_holidays(holiday_hash)
   holiday_hash.each do |season, holidays|
     puts "#{season.to_s.capitalize}:"
     holidays.each do |holiday, supplies|
-      puts "  #{holiday.to_s.tr("_", " ").split.capitalize}: #{supplies.join(", ")}"
+      puts "  #{holiday.to_s.tr("_", " ").capitalize}: #{supplies.join(", ")}"
     end
   end
 end
